56036821f754d7b3ad3e314f727664c9a3b6a3fe
Amitai <git@amitai.ga>
2020-08-22 01:23:06 +0100

    implemented post names

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Post(models.Model):
     content = models.TextField(verbose_name='')
     author = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content[:12].strip() + ('...' if len(self.content) > 10 else '')
