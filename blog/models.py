34a5a71359f5059c4815dd3515c549d52ccf9534
Amitai <git@amitai.ga>
2020-08-30 18:00:06 +0100

    changed newlines

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-
 MAX = 14
+
 
 class Post(models.Model):
     title = models.CharField(max_length=255, blank=True, default="", verbose_name='')
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
+
 def index(request):
     return render(request, 'blog/index.html',
         {'posts': Post.objects.order_by('-pin', '-date')})
